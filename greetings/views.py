a5f346d6cb62cf22938a9099d54a50c4a9deef93
Sanzhar <salergame07@gmail.com>
2025-03-07 20:43:25 +0500

    Добавлена функция test в views.py

--- greetings/views.py
+++ greetings/views.py
@@ -14,4 +14,8 @@ def mama(request):
 
 def sister(request):
     """Представление для страницы поздравления сестры."""
-    return render(request, 'greetings/sister.html') 
+    return render(request, 'greetings/sister.html')
+
+def test(request):
+    """Тестовое представление для проверки работоспособности сайта."""
+    return render(request, 'greetings/test.html') 
