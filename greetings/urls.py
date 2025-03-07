8b66911a9e53e031f8c0617a3a8ac712f6da4980
Sanzhar <salergame07@gmail.com>
2025-03-07 20:40:09 +0500

    Добавлено перенаправление с /greetings/ на корневой URL

--- greetings/urls.py
+++ greetings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('mama/', views.mama, name='mama'),
     path('sister/', views.sister, name='sister'),
+    path('test/', views.test, name='test'),
 ] 
--- march8project/urls.py
+++ march8project/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('greetings.urls')),  # Добавляем маршруты приложения greetings
+    path('greetings/', RedirectView.as_view(url='/')),  # Перенаправление с /greetings/ на корневой URL
 ]
+
+# Добавляем обработку статических файлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
